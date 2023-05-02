848d0171543f28e006618aba44b5ea86468a6954
Malte Weinberg <malte.weinberg@kuehne-nagel.com>
2023-05-02 19:59:15 +0200

    Test dockerfile

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -ex
+
+### Aliases #######################################
+
+# listing:
+echo 'alias ll="ls -lha"' >> $HOME/.bashrc
+echo 'alias la="ls -A"' >> $HOME/.bashrc
+echo 'alias l="ls -CF"' >> $HOME/.bashrc
+
+# directories:
+alias cdd="cd ../.."
+alias cddd="cd ../../.."
+alias cd..="cd .."
+
+# typos:
+alias sl="ls"
+alias c="clear"
+alias h="history"
+
+# root:
+alias root="sudo -i"
+alias su="sudo -i"
+
+# grep:
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+alias grepi="grep -ri"
+alias greph="grep -ri --exclude-dir='.*'"
+alias gn="grep -ri --exclude='*.ipynb'"
+
+# git:
+alias gdd="git add"
+alias glg="git log --oneline --decorate --graph --all"
+alias gst="git status"
+alias gco="git checkout"
+alias gcm="git commit"
+
+
+#### Poetry #######################################
+WORKSPACE_DIR=$(pwd)
+poetry config cache-dir ${WORKSPACE_DIR}/.cache
+poetry config virtualenvs.in-project true
+poetry install
+
+echo "Done!"
--- README.md
+++ README.md
@@ -4,8 +4,6 @@ An introduction into time series forecasting basics, challenges, best practices.
 ## Getting Started
 This workshop is designed to be run within GitHub Codespaces.
 
-* Go to the [GitHub repository](https://github.com/WeinbergMalte/timeseries-forecasting-workshop),
+* Go to the [GitHub repository](https://github.com/WeinbergMalte/timeseries-forecasting-workshop)
 * Click on "Code" and select "Codespaces"
 * Click the "+" to create a new codespace
-
-
