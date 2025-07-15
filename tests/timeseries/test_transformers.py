3319a0d737235f32952cfd1bfda6e9b36ff27031
Malte Weinberg <malte.weinberg@kuehne-nagel.com>
2025-07-15 10:21:11 +0200

    Ruff formatting

--- tests/timeseries/test_transformers.py
+++ tests/timeseries/test_transformers.py
@@ -1,9 +1,8 @@
-import pytest
-
 from typing import List
 
 import numpy as np
 import pandas as pd
+import pytest
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal
 
